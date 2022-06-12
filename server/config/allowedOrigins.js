ed4b77a8340b4141bb4e7ddf1b23c311aa69259e
Tom Ari <freak.tom3@gmail.com>
2022-06-12 14:19:26 +0300

    access route

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,3 @@
-const allowedOrigins = [
-  "https://mern-blog-2.herokuapp.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:8000",
-  "http://localhost:3000",
-];
+const allowedOrigins = ["https://mern-blog-2.herokuapp.com"];
 
 module.exports = allowedOrigins;
