1f7172a75a1a7a6356c325207ae08f0b182160a3
Tom Ari <freak.tom3@gmail.com>
2022-06-12 14:53:52 +0300

    static after routes

--- server/app.js
+++ server/app.js
@@ -28,16 +28,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
-}
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", require("./routes/auth"));
@@ -47,6 +37,15 @@ app.use("/posts", require("./routes/api/posts"));
 app.use("/users", require("./routes/api/users"));
 app.use("/tags", require("./routes/api/tags"));
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 // app.use(require("./routes/api"));
 
 // catch 404 and forward to error handler
