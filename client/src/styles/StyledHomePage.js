8756ccc2a57166a9e7c3fa32bc383408bfcfc3c9
Tom Ari <freak.tom3@gmail.com>
2022-06-13 15:57:20 +0300

    sticky

--- client/src/styles/StyledHomePage.js
+++ client/src/styles/StyledHomePage.js
@@ -5,11 +5,15 @@ export const StyledHomePage = styled(motion.main)`
   display: flex;
 
   .side-panels {
+    position: relative;
     display: flex;
     flex-direction: column;
     gap: var(--paddingBig);
 
     section {
+      position: sticky;
+      top: var(--paddingBigger);
+
       min-width: 300px;
     }
   }
