b19ec0cd5913eb321bbda5f6eb7aa011b8c9105a
Tom Ari <freak.tom3@gmail.com>
2022-06-19 16:57:41 +0300

    removed images from tags

--- client/src/components/tags/TagBanner.js
+++ client/src/components/tags/TagBanner.js
@@ -6,9 +6,6 @@ export const TagBanner = ({ tagData }) => {
   return (
     <StyledBanner className="TagBanner">
       <div className="banner">
-        <div className="banner_image">
-          <div className="img"></div>
-        </div>
         <div className="banner_info">
           <Tag tagData={tagData} />
         </div>
--- client/src/styles/StyledPanel.js
+++ client/src/styles/StyledPanel.js
@@ -3,18 +3,16 @@ import styled from "styled-components";
 export const StyledPanel = styled.section`
   background-color: var(--primaryLighter);
   border-radius: var(--radius);
+  box-shadow: var(--cardShadowXSmall);
+
   padding: var(--padding);
   width: 300px;
-  box-shadow: var(--cardShadowXSmall);
   h1 {
     font-size: var(--fontSizeMedium);
     padding: var(--paddingSmall);
   }
-
-  .user-panel_users {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: var(--paddingBig);
+  div {
+    display: grid;
+    grid-template-rows: repeat(auto-fit, 60px);
   }
 `;
